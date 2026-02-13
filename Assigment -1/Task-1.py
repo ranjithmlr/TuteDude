67df892ce34c87fc5a3b0fa8bdd6d689b9506f59
Ranjith_C_Mlr <ranjithc1601@gmail.com>
2026-02-14 00:48:16 +0530

    Assignment -1
    Module 2: Basic Python Concepts

---
+++ Assigment -1/Task-1.py
@@ -0,0 +1,15 @@
+# Take a two integer inputs
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
